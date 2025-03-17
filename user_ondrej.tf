e4d3efc6b9342f82ed1cf0e508f93043b659cfee
Ondrej Sika <ondrej@ondrejsika.com>
2025-03-17 14:22:19 +0100

    feat: Create user ondrej

---
+++ user_ondrej.tf
@@ -0,0 +1,21 @@
+resource "keycloak_user" "ondrej" {
+  lifecycle {
+    ignore_changes = [
+      required_actions,
+    ]
+  }
+
+  realm_id = keycloak_realm.example.id
+  username = "ondrej"
+  enabled  = true
+
+  email          = "ondrej@example.com"
+  email_verified = true
+  first_name     = "Ondrej"
+  last_name      = "Sika"
+
+  initial_password {
+    value     = "a"
+    temporary = true
+  }
+}
