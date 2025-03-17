6384a97552fc82e0e8690ab483e005b8e1850904
Ondrej Sika <ondrej@ondrejsika.com>
2025-03-17 15:53:33 +0100

    feat: Create example OIDC client

---
+++ client_example.tf
@@ -0,0 +1,17 @@
+resource "keycloak_openid_client" "example" {
+  realm_id                        = keycloak_realm.example.id
+  client_id                       = "example"
+  client_secret                   = "example"
+  enabled                         = true
+  standard_flow_enabled           = true
+  direct_access_grants_enabled    = true
+  access_type                     = "CONFIDENTIAL"
+  valid_redirect_uris             = ["*"]
+  valid_post_logout_redirect_uris = ["*"]
+  web_origins                     = ["*"]
+}
+
+output "example_client_secret" {
+  value     = keycloak_openid_client.example.client_secret
+  sensitive = true
+}
