a8c071cf04dd8b903bc49cc718ef6ffcfe22a0dd
Ondrej Sika <ondrej@ondrejsika.com>
2025-03-18 10:45:00 +0100

    feat: Create client roles for example client

--- client_example.tf
+++ client_example.tf
@@ -11,6 +11,18 @@ resource "keycloak_openid_client" "example" {
   web_origins                     = ["*"]
 }
 
+resource "keycloak_role" "example_editor" {
+  realm_id  = keycloak_realm.example.id
+  client_id = keycloak_openid_client.example.id
+  name      = "editor"
+}
+
+resource "keycloak_role" "example_viewer" {
+  realm_id  = keycloak_realm.example.id
+  client_id = keycloak_openid_client.example.id
+  name      = "viewer"
+}
+
 output "example_client_secret" {
   value     = keycloak_openid_client.example.client_secret
   sensitive = true
